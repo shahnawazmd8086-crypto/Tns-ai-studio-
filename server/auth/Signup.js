83de95e2a7b566362fc846a471303bfdaec9bad2
shahnawazmd8086-crypto <shahnawazmd8086@gmail.com>
2026-09-12 12:20:51 +0530

    Update Signup.js

--- server/auth/Signup.js
+++ server/auth/Signup.js
@@ -1,1 +1,70 @@
+const {
+  createUser,
+  normalizeEmail,
+  findUserByEmail,
+  validatePassword
+} = require("./Auth");
 
+
+function validateSignupInput(input = {}) {
+  const email = normalizeEmail(input.email);
+  const password = String(input.password || "");
+
+  if (!email) {
+    throw new Error("Email is required.");
+  }
+
+  if (!validatePassword(password)) {
+    throw new Error(
+      "Password must be at least 8 characters long."
+    );
+  }
+
+  return {
+    email,
+    password
+  };
+}
+
+
+function signup(input = {}) {
+  const {
+    email,
+    password
+  } = validateSignupInput(input);
+
+  if (findUserByEmail(email)) {
+    throw new Error("User already exists.");
+  }
+
+  const user = createUser({
+    email,
+    password
+  });
+
+  return {
+    success: true,
+    user
+  };
+}
+
+
+function isEmailAvailable(email) {
+  const normalizedEmail =
+    normalizeEmail(email);
+
+  if (!normalizedEmail) {
+    return false;
+  }
+
+  return !findUserByEmail(
+    normalizedEmail
+  );
+}
+
+
+module.exports = {
+  validateSignupInput,
+  signup,
+  isEmailAvailable
+};

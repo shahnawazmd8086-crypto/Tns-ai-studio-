37e3f0c886f120507927ac771386ad5a58a119e3
shahnawazmd8086-crypto <shahnawazmd8086@gmail.com>
2026-09-12 13:28:26 +0530

    Update editor.js

--- public/components/editor.js
+++ public/components/editor.js
@@ -1,1 +1,50 @@
+const TNSEditorComponent = {
+  state: {
+    currentProjectId: null,
+    timeline: [],
+    selectedItemId: null,
+    isPlaying: false,
+    currentTime: 0
+  },
 
+  getState() {
+    return {
+      ...this.state,
+      timeline: [...this.state.timeline]
+    };
+  },
+
+  setProject(projectId) {
+    this.state.currentProjectId =
+      projectId || null;
+
+    return this.state.currentProjectId;
+  },
+
+  addItem(item = {}) {
+    const newItem = {
+      id:
+        item.id ||
+        `editor-item-${Date.now()}-${Math.random()
+          .toString(36)
+          .slice(2, 8)}`,
+
+      type:
+        item.type ||
+        "video",
+
+      name:
+        item.name ||
+        "Untitled Item",
+
+      src:
+        item.src ||
+        "",
+
+      start:
+        Number(item.start) || 0,
+
+      duration:
+        Number(item.duration) || 0,
+
+      trim
